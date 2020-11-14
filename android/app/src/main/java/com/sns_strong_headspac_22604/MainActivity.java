8a2b1300047055e87e6da85ffc49ca703e8ce075
Crowdbotics <team@crowdbotics.com>
2020-11-14 00:58:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sns_strong_headspac_22604/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sns_strong_headspac_22604;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "sns_strong_headspac_22604";
+    }
+}
